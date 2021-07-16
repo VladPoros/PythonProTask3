f0fa310aa77160b6f7c4105ca3065679d17b025f
VladPoros <pve9512@gmail.com>
2021-07-16 14:03:21 +0300

    Task3UPD

--- Task3.py
+++ Task3.py
@@ -4,14 +4,22 @@ import sqlite3
 
 app = Flask(__name__)
 
-'''
-connection = sqlite3.connect('blog.sqlite')
-cursor = connection.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS sections (id integer primary key AUTOINCREMENT, 
-title varchar(200), description varchar(200), date int)""")
-connection.commit()
-connection.close()
-'''
+
+def BD(execute):
+    connection = sqlite3.connect('blog.sqlite')
+    cursor = connection.cursor()
+    cursor.execute(execute)
+    sections = cursor.fetchall()
+    connection.close()
+    return render_template('index.html', sections=sections)
+
+def BD_amend (execute, values):
+    connection = sqlite3.connect('blog.sqlite')
+    cursor = connection.cursor()
+    cursor.execute(execute, values)
+    connection.commit()
+    connection.close()
+    return redirect('/info')
 
 @app.route('/')
 def start_page():
@@ -25,12 +33,7 @@ def info_page():
     Use this page after redirect each action
     Also use index html for templates
     '''
-    connection = sqlite3.connect('blog.sqlite')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM sections")
-    sections = cursor.fetchall()
-    connection.close()
-    return render_template('index.html', sections=sections)
+    return BD("SELECT * FROM sections")
 
 
 @app.route('/add/', methods=('GET', 'POST'))
@@ -48,20 +51,12 @@ def add_page():
         added_description = request.form['description']
         if not added_title and not added_description:
             return 'You don`t enter the title and description'
-        if not added_title:
-            return 'You don`t enter the title'
-        if not added_description:
-            return 'You don`t enter the description'
-
+        if not added_title or not added_description:
+            return 'You don`t enter the title or description'
         else:
-            connection = sqlite3.connect('blog.sqlite')
-            cursor = connection.cursor()
             values = (added_title, added_description)
-            cursor.execute("""INSERT INTO sections (title, description, date) 
+            return BD_amend("""INSERT INTO sections (title, description, date) 
             VALUES(?, ?, '15/07')""", values)
-            connection.commit()
-            connection.close()
-            return redirect('/info')
 
 
 @app.route('/edit/', methods=('GET', 'POST'))
@@ -81,29 +76,18 @@ def edit_page():
         if not id:
             return 'You don`t enter the id or error id'
         else:
-            connection=sqlite3.connect('blog.sqlite')
-            cursor = connection.cursor()
             if not updated_title:
                 values = (updated_description, id)
-                cursor.execute("""UPDATE sections SET description = ?
+                return BD_amend("""UPDATE sections SET description = ?
                             WHERE id = ?""", values)
-                connection.commit()
-                connection.close()
-                return redirect('/info')
             if not updated_description:
                 values = (updated_title, id)
-                cursor.execute("""UPDATE sections SET title = ?
+                return BD_amend("""UPDATE sections SET title = ?
                             WHERE id = ?""", values)
-                connection.commit()
-                connection.close()
-                return redirect('/info')
             else:
                 values = (updated_title, updated_description, id)
-                cursor.execute("""UPDATE sections SET title = ?, description = ?
+                return BD_amend("""UPDATE sections SET title = ?, description = ?
                 WHERE id = ?""", values)
-                connection.commit()
-                connection.close()
-                return redirect('/info')
 
 
 @app.route('/delete/', methods=('GET', 'POST'))
@@ -120,13 +104,8 @@ def delete_page():
         if not id:
             return 'You don`t enter the id'
         else:
-            connection = sqlite3.connect('blog.sqlite')
-            cursor = connection.cursor()
             values = (id)
-            cursor.execute("DELETE FROM sections WHERE id = ?", values)
-            connection.commit()
-            connection.close()
-            return redirect('/info')
+            return BD_amend("DELETE FROM sections WHERE id = ?", values)
 
 
 if __name__ == '__main__':
